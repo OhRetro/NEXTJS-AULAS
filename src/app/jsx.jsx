a1ef30309d90e006fe29feee178b144ba65520f3
OhRetro <gabrielnacazone@hotmail.com>
2023-06-19 12:14:59 -0300

    19.6.23

---
+++ src/app/jsx.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Jsx() {
+    return (
+        <div>
+            <h1>JSX</h1>
+        </div>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Next.js & React</h1>
+            <h2>Framework</h2>
+        </div>
+    )
+}
